674cb5e619da1ec8e3d2efe7d3b2742a861cc562
nariyu <nariyu@playwell.jp>
2021-04-17 16:35:27 +0900

    Change og:image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
       <meta property="og:site_name" content={TITLE} />
       <meta property="og:title" content={TITLE} />
       <meta property="og:description" content={DESCRIPTION} />
-      <meta property="og:image" content={`${URL}/assets/ogp.png`} />
+      <meta property="og:image" content={`${URL}/assets/nariyuogp.png`} />
       <meta property="og:url" content={`${URL}/`} />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="ja_JP" />
@@ -21,7 +21,7 @@ const Home = () => {
       <meta name="twitter:url" content={`${URL}/`} />
       <meta name="twitter:title" content={TITLE} />
       <meta name="twitter:description" content={DESCRIPTION} />
-      <meta name="twitter:image" content={`${URL}/assets/ogp.png`} />
+      <meta name="twitter:image" content={`${URL}/assets/nariyuogp.png`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={TITLE} />
     </Head>
